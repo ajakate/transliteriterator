0df38867efb3de9185cacca2c269721f24b02586
ajakate <ajay.jakate@gmail.com>
2014-02-11 19:09:38 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transliteriterator::Application.config.secret_key_base = '1203daabf7cbbae2f3334463120b856abb6219b84a2e32e3a00932f67621257e3f63554cac1df53c4a18b7aa39e04da3d3371ba039a4c2f652133d960cf5b876'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transliteriterator::Application.config.session_store :cookie_store, key: '_transliteriterator_session'
